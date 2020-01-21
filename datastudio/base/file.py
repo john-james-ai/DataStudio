df2ff6659e90db9605dae79c902fcd5c08b2050f
John James <jjames@decisionscients.com>
2020-01-20 20:12:57 -0500

    File classes implemented

--- datastudio/base/file.py
+++ datastudio/base/file.py
@@ -42,6 +42,8 @@ The FileIO family of classes include:
 from abc import ABC, abstractmethod
 import os
 import shutil
+
+import numpy as np
 import pandas as pd
 
 class FileObject(ABC):
@@ -52,7 +54,7 @@ class FileObject(ABC):
         self._path = path
         self._directory = os.path.dirname(path)
         self._locked = False
-        self._exists = True
+        self._exists = os.path.exists(path)
 
     @property
     def name(self):
@@ -180,8 +182,14 @@ class File(FileObject):
         self._update_filename_data(new_path)
         return new_path
 
-    def read(self):
+    def read(self, columns=None):
         """Reads and returns the file contents.
+
+        Parameters
+        ----------
+        subset : array like (Optional)
+            Used when reading .csv or .csv.gz files. Specifies specific 
+            columns to read.
 
         Returns
         -------
@@ -200,7 +208,7 @@ class File(FileObject):
 
         """
         io = FileIOFactory()
-        return io.read(self._path)
+        return io.read(self._path, columns)
 
     def write(self, content):
         """Writes content to file.
@@ -220,145 +228,354 @@ class File(FileObject):
         io = FileIOFactory()
         io.write(self._path, content)
 
+# ---------------------------------------------------------------------------- #
+#                                   FILE GROUP                                 #   
+# ---------------------------------------------------------------------------- #
+class FileGroup(FileObject):
+    """Collection of File and FileGroup objects.
+    
+    Note
+    ----
+    File and FileGroup objects are stored as dictionaries. No two 
+    objects may have the same name. 
+    """
+
+    def __init__(self, path, name=None):
+        super(File, self).__init__(path=path, name=name)
+        self._file_objects = {}
+
+    def get(name):
+        """Gets the FileObject designated by 'name'.
+        
+        Parameters
+        ----------
+        name : str
+            The name of the FileObject to obtain
+        """
+        try:
+            result = self._file_objects[name]
+        except KeyError:
+            print("No object of the name {name} exists in the FileGroup.\
+                ".format(name=name))
+
+    def add(file_object):
+        """Adds a File or FileGroup
+
+        Parameters
+        ----------
+        file_object : File or FileGroup
+            The object to be added to the FileGroup object.
+        
+        """
+
+        name = file_object.name
+        try:
+            self._file_objects[name] = file_object
+        except KeyError:
+            print("An object with the same name, {name}, already exists in the \
+                FileGroup.".format(name=name))
+
+    def remove(name):
+        """Removes a FileObject designated by 'name' from the FileGroup."""
+        try:
+            del self._file_objects[name]
+        except KeyError:
+            print("No object of the name {name} exists in the FileGroup.\
+                ".format(name=name))
+
+    def print():
+        """Prints a list of the FileObjects contained herein."""
+        if self._file_objects:
+            d = {}
+            classnames = []
+            names = []
+            paths = []
+            created = []
+            updated = []
+            sizes = []
+
+            for name, file_object in self._file_objects.items():
+                classnames.append(file_object.__class__.__name__)
+                names.append(file_object.name)
+                paths.append(file_object.path)
+                created.append(file_object.created)
+                updated.append(file_object.updated)
+                sizes.append(file_object.size)
+            d = {
+                'Class' : classnames,
+                'Name' : names,
+                'Path' : paths,
+                'Created' : created,
+                'Last Updated' : updated,
+                'Size' : sizes
+            }
+            df = pd.DataFrame(d)
+            print(tabulate(df))
+        else:
+            print("This FileGroup contains no FileObjects.")
+    
+
+    
+
+
 
 # ---------------------------------------------------------------------------- #
 #                                FILEIO FACTORY                                #   
 # ---------------------------------------------------------------------------- #
 class FileIOFactory:
     """Encapsulates an individual file on disk."""
-    _FILE_HANDLERS = {'gz': FileGZ(), 'csv': FileCSV()}
+    _FILE_HANDLERS = {'.gz': FileCSVgz(), '.csv': FileCSV(), '.npy': FileNumpy()}
 
     def __init__(self):
         pass
         
-    def _get_file_handler(self, filename):
-        file_ext = filename.split(".")[-1]
+    def _get_file_handler(self, path):
+        file_ext = os.path.splitext(path)[1]
         file_handler = self._FILE_HANDLERS.get(file_ext)
         if file_handler is None:
             raise Exception("{ext} files are not supported.".format(ext=file_ext))        
         else:
             return file_handler
 
-    def read(self, filename, columns=None):
+    def read(self, path, columns=None):
         """Obtains a file handler based upon the file extension, then reads.""" 
-        file_handler = self._get_file_handler(filename)
-        return file_handler.read(filename, columns)
+        file_handler = self._get_file_handler(path)
+        return file_handler.read(path, columns)
 
-    def write(self, filename, df):
+    def write(self, path, df):
         """Obtains a file handler based upon the file extension, then reads.""" 
-        file_handler = self._get_file_handler(filename)
-        return file_handler.write(filename, df)
+        file_handler = self._get_file_handler(path)
+        return file_handler.write(path, df)
+
 # ---------------------------------------------------------------------------- #
-#                               FileGZ                                         #  
+#                                 FILEIO                                       #  
 # ---------------------------------------------------------------------------- #
-class FileGZ(BaseFile):
-    """Read and write compressed GZ files and returning DataFrames."""
+class FileIO:
+    """Abstract base class for FileIO subclasses."""
 
     def __init__(self):
         pass
 
-    def read(self, filename, columns=None):
-        """Reads a .gz file, designated by 'filename' into a DataFrame.
+    def _check_dir(path):
+        """Checks existence of a files directory and creates if not exists."""
+
+        directory = os.path.dirname(path)
+        if not os.path.exists(path):
+            os.mkdir(directory)    
+            print("Directory did not exist. \
+                Created {dirname}.".format(dirname=directory))
+
+    def _check_file_ext(path, ext):
+        """Ensures file extension is correct."""
+        if not os.path.splitext(path)[1] == ext:
+            new_path = os.path.splitext(path)[0] + ext
+            print("File extension incompatible with file type.\
+                Saving {oldname} as {newname}.".format(
+                    oldname=path, newname=new_path
+                ))
+            return new_path
+        return path
+        
+#         
+# ---------------------------------------------------------------------------- #
+#                               FilECSVGZ                                      #  
+# ---------------------------------------------------------------------------- #
+class FileCSVgz:
+    """Read and write .gz compressed CSV files into and from DataFrame objects."""
+
+    def __init__(self):
+        pass
+
+    def read(self, path, columns=None):
+        """Reads a .gz file, designated by 'path' into a DataFrame.
         
         Parameters
         ----------
-        filename : str
+        path : str
             The relative or fully qualified file path
         columns : list
             A list of column names to return
 
         Returns
         -------
-        DataFrame : The file contents in DataFrame format.
+        DataFrame : The file contents in DataFrame format. Returns None if 
+                    unable to read the file.
         
         """
-
-        self._filename = filename
-        df = pd.read_csv(filename, compression='gzip', error_bad_lines=False,
+        
+        try:
+            result = pd.read_csv(path, compression='gzip', error_bad_lines=False,
                         low_memory=False, usecols=columns)
-        return df
+        except IOError:
+            print("The file, {fname}, does not exist. None returned.".format(fname=path))
+            result = None
+        except Exception as e:
+            print(e)
+            result = None
+        return result
 
-    def write(self, filename, df):
-        """Accepts a filename and a DataFrame and writes it to a .gz file.
+    def write(self, path, content):
+        """Accepts a path and a DataFrame and writes it to a .csv.gz file.
         
         Parameters
         ----------
-        filename : str
+        path : str
             The relative or fully qualified file path
-        df : DataFrame
-            The DataFrame object to be written to file.
+        content : DataFrame
+            The content to be written to file.
 
         Returns
         -------
-        self
+        str
+            If successful, the method returns the path to which the file was
+            written.  If unsuccessful, None is returned.
         
         """
+        
+        self._check_dir(path)
+        path = self._check_file_ext(path, '.gz')
+        try:
+            content.to_csv(path, compression='gzip')
+        except Exception as e:
+            print(e)
+            path = None
 
-        self._filename = filename
-        self._df = df
-        check_dir(filename)
-        df.to_csv(filename, compression='gzip')
-        return self
+        return path
         
 # ---------------------------------------------------------------------------- #
 #                               FileCSV                                        #  
 # ---------------------------------------------------------------------------- #
-class FileCSV(BaseFile):
+class FileCSV:
     """Read and write CSV files and returning DataFrames."""
 
     def __init__(self):
         pass
 
-    def read(self, filename, columns=None):
-        """Reads a .csv file, designated by 'filename' into a DataFrame.
+    def read(self, path, columns=None):
+        """Reads a .csv file, designated by 'path' into a DataFrame.
         
         Parameters
         ----------
-        filename : str
+        path : str
             The relative or fully qualified file path
         columns : list
             A list of column names to return
 
         Returns
         -------
-        DataFrame : The file contents in DataFrame format.
+        DataFrame : The file contents in DataFrame format. Returns None if 
+                    unable to read the file.
         
         """
+        try:
+            result = pd.read_csv(path, usecols=columns)
+        except IOError:
+            print("The file, {fname}, does not exist. None returned.".format(fname=path))
+            result = None
+        except Exception as e:
+            print(e)
+            result = None
+        return result
 
-        self._filename = filename
-        df = pd.read_csv(filename, usecols=columns)
-        return df
-
-    def write(self, filename, df):
+    def write(self, path, content):
         """Accepts a filename and a DataFrame and writes it to a .csv file.
         
         Parameters
         ----------
-        filename : str
+        path : str
             The relative or fully qualified file path
-        df : DataFrame
+        content : DataFrame
             The DataFrame object to be written to file.
 
         Returns
         -------
-        self
-        
+        str
+            If successful, the method returns the path to which the file was
+            written.  If unsuccessful, None is returned.
+
         """
 
-        self._filename = filename
-        self._df = df
-        check_dir(filename)
-        df.to_csv(filename)
-        return self
+        self._check_dir(path)
+        path = self._check_file_ext(path, '.csv')
+        try:
+            content.to_csv(path)
+        except Exception as e:
+            print(e)
+            path = None
+        return path
         
 
-
 # ---------------------------------------------------------------------------- #
-#                                    HELPERS                                   #   
-# ---------------------------------------------------------------------------- #        
-def check_dir(filename):
-    directory = os.path.dirname(filename)
-    if os.path.exists(directory):
+#                               FileNumpy                                      #  
+# ---------------------------------------------------------------------------- #
+class FileNumpy:
+    """Read and write numpy files (.npy, .npz)"""
+
+    def __init__(self):
         pass
-    else:
-        os.mkdir(directory)
+
+    def read(self, path, columns=None):
+        """Reads a .npy or .npz file, designated by 'path' into a numpy object.
+
+        For .npy files, a numpy array is returned. For .npz files, a dictionary
+        is returned containing the filename as key and a numpy array as 
+        the value, for each file in the archive.
+        
+        Parameters
+        ----------
+        path : str
+            The relative or fully qualified file path
+        columns : array-like
+            Not used
+
+        Returns
+        -------
+        numpy array / dict
+            Numpy array for .npy files. Dictionary for .npz files. Returns 
+                None if unable to read the file.
+        
+        """
+        try:
+            result = np.load(path)
+        except IOError:
+            print("The file, {fname}, does not exist. None returned.".format(fname=path))
+            result = None
+        except Exception as e:
+            print(e)
+            result = None
+        return result
+
+    def write(self, path, content):
+        """Writes a numpy array, or a dictionary containing, to path.
+        
+        Parameters
+        ----------
+        path : str
+            The relative or fully qualified file path
+        content : numpy array or array like thereof
+            A numpy array or a dictionary containing numpy arrays.
+
+        Returns
+        -------
+        str
+            If successful, the method returns the path to which the file was
+            written.  If unsuccessful, None is returned.
+        
+        """
+        
+        self._check_dir(path)        
+        if isinstance(content, (np.ndarray, np.generic)):
+            path = self._check_file_ext(path, '.npy')
+            try:                
+                np.save(path, content)
+            except Exception as e:
+                print(e)
+                path = None
+        else:
+            path = self._check_file_ext(path, '.npz')
+            try:
+                np.savez(path, content)
+            except Exception as e:
+                print(e)
+                path = None        
+        return path
