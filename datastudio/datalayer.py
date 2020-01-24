9bd9735da2241d443253e9d2cce7c7fa166c6451
John James <jjames@decisionscients.com>
2020-01-24 11:16:27 -0500

    Added logging. Getting connection via sqlalchemy

--- datastudio/datalayer.py
+++ datastudio/datalayer.py
@@ -19,11 +19,15 @@
 # =========================================================================== #
 """Module encapsulating all PostgreSQL data operations."""
 from abc import ABC, abstractmethod
+import logging
 import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from sqlalchemy import create_engine
+import sys
+import yaml
 
-from credentials import USERID, PASSWORD, HOST, PORT, SERVER
+CREDENTIALS_FILENAME = 'credentials.yaml'
 # --------------------------------------------------------------------------- #
 #                                DATALAYER                                    #
 # --------------------------------------------------------------------------- #
@@ -31,12 +35,35 @@ class DataLayer(ABC):
     """Abstract base layer for DataLayer objects."""
 
     def __init__(self, name):
-        self._name = name
-        self._userid = USERID
-        self._pwd = PASSWORD
-        self._host = HOST
-        self._port = PORT
-        self._server = SERVER
+        self._name = name                 # Name for new database or table.
+        self._server_connection = None    # Connection to PostgreSQL database
+        self._db_connection=None          # Connection to new named database
+        
+        # Instantiate logger
+        self._log = logging.getLogger()
+        self._log.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self._log.addHandler(handler)
+
+        # Obtain PostgreSQL credentials.
+        with open(CREDENTIALS_FILENAME) as f:
+            vals = yaml.load(f, Loader=yaml.FullLoader)
+
+        if ('USERID' not in vals.keys() or
+            'PASSWORD' not in vals.keys() or
+            'DBNAME' not in vals.keys() or
+            'HOST' not in vals.keys() or
+            'PORT' not in vals.keys()):
+            raise ValueError('Bad credentials file: ' + CREDENTIALS_FILENAME)
+
+        self._userid = vals['USERID']
+        self._pwd = vals['PASSWORD']
+        self._pg_dbname = vals['DBNAME']
+        self._host = vals['HOST']
+        self._port = vals['PORT']
 
     @property
     def name(self):
@@ -59,78 +86,77 @@ class DataLayer(ABC):
         pass
 
 # --------------------------------------------------------------------------- #
-#                                DATALAYER                                    #
+#                                DATABASE                                     #
 # --------------------------------------------------------------------------- #
 class Database(DataLayer):
     """Creates & manages PostgreSQL databases and contains DataTable objects.""" 
 
     def __init__(self, name):
         super(Database, self).__init__(name)
-        self._server_connection = None
-        self._db_connection = None
-        self._cursor = None
-        self._data_entities = {}
 
     def _connect(self, name):
-        """Creates a connection to PostgreSQL server."""
-        print("Connecting to {name}...\n".format(name=name))
         try:
-            connection = psycopg2.connect(user = self._userid,
-                                          password = self._pwd,
-                                          host = self._host,
-                                          port = self._port,
-                                          database = name)            
-        except (Exception, psycopg2.Error) as error: 
-            print("Error while connecting to PostgreSQL. {error}".format(
-                error=error))
+            engine = self._get_connection(name)
+            self._log.info("Connected to {name} database!".format(name=name))
+        except IOError:
+            self._log.exception("Failed to connect to {name} database!".format(name=name))
             return None
-        # Print PostgreSQL cursor properties
-        print(connection.get_dsn_parameters(), "\n")            
-        return connection
+        return engine
 
-    def _get_server_connection(self):
-        self._server_connection = self._connect(self._server)
-        self._server_connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)        
+    def _get_connection(self, name):
+        """Connects to named database."""
+        
+        url = "postgresql://{user}:{passwd}@{host}:{port}/{db}".format(
+            user=self._userid, passwd=self._pwd, host=self._host, 
+            port=self._port, db=name)
+      
+        engine = create_engine(url, pool_size=50)
+        return engine
+         
+    def get_server_connection(self):
+        self._server_connection = self._connect(self._pg_dbname).raw_connection()      
+        return self._server_connection  
 
-    def _get_db_connection(self):
-        self._db_connection = self._connect(self._name)
+    def get_db_connection(self):
+        self._db_connection = self._connect(self._name).raw_connection()
+        return self._db_connection
 
-
-    def _close_connection(self, connection):
+    def close_connection(self, connection):
         """Closes connection."""
-        print("Closing PostgreSQL connection.")
+        self._log.info("Closing PostgreSQL connection.")
         if (connection):
             connection.close()
-            print("PostgreSQL connection is closed.")
+            self._log.info("PostgreSQL connection is closed.")
 
-    def _close_cursor(self, cursor):
+    def close_cursor(self, cursor):
         """Closes cursor."""
-        print("Closing PostgreSQL cursor.")
+        self._log.info("Closing PostgreSQL cursor.")
         if (cursor):
             cursor.close()
-            print("PostgreSQL cursor is closed.")
+            self._log.info("PostgreSQL cursor is closed.")
 
     def _terminate_connections(self, name):
         """Terminates active connections to databases."""
         # Assumes connection and cursor to server. 
-        print("Terminating active connections.")
+        self._log.info("Terminating active connections.")
         try:
             self._cursor.execute(\
                 "SELECT pg_terminate_backend(pg_stat_activity.pid)\
                     FROM pg_stat_activity \
                         WHERE pg_stat_activity.datname = %s;", (name,))
-            print("Active connections on {name} terminated.".format(
+            self._log.info("Active connections on {name} terminated.".format(
                 name=name
             ))
         except (Exception, psycopg2.Error) as error:
-            print("Error terminating active connections. {error}"\
+            self._log.exception("Error terminating active connections. {error}"\
                 .format(error=error))
 
     def create(self):
         """Creates a PostgreSQL database."""
         # Connect to PostgreSQL DBMS
-        print("Creating {name} database.".format(name=self._name))
-        self._get_server_connection()
+        self._log.info("Creating {name} database.".format(name=self._name))
+        _ = self.get_server_connection()
+        self._server_connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  
         self._cursor = self._server_connection.cursor()
         try:
             self._cursor.execute(sql.SQL("CREATE DATABASE {}").format(
@@ -138,32 +164,31 @@ class Database(DataLayer):
             # Print PostgreSQL Database version
             self._cursor.execute('SELECT version()')
             db_version = self._cursor.fetchone()
-            print(db_version)                    
+            self._log.infor(db_version)                    
         except (Exception, psycopg2.Error) as error:
-            print("Error creating databases {db}. {error}".format(
+            self._log.info("Error creating databases {db}. {error}".format(
                         db=self._name, error = error))
         finally:
-            self._close_connection(self._server_connection)            
-            self._close_cursor(self._cursor)
+            self.close_connection(self._server_connection)            
+            self.close_cursor(self._cursor)
             
 
     def drop_db(self):
         """Drops the PostgreSQL database."""
-        self._get_server_connection()
+        _ = self.get_server_connection()
         self._cursor = self._server_connection.cursor()
         self._terminate_connections(self._name)
         try:
             self._cursor.execute(sql.SQL("drop database if exists {}").format(
                 sql.Identifier(self._name)))
-            print("Successfully dropped {name} database.".format(
+            self._log.info("Successfully dropped {name} database.".format(
                 name=self._name))
         except (Exception, psycopg2.Error) as error:
-            print("Error dropping the {name} database. {error}".format(
+            self._log.info("Error dropping the {name} database. {error}".format(
                 name=self._name, error=error))
         finally:
-            self._close_connection(self._server_connection)
-            self._close_cursor(self._cursor)
-
+            self.close_connection(self._server_connection)
+            self.close_cursor(self._cursor)
 
     def get(self, name):
         """Retrieves a member Database or DataTable object.
@@ -201,5 +226,20 @@ class Database(DataLayer):
         del self._data_entities[name]
         return self
     
+# --------------------------------------------------------------------------- #
+#                                DATATABLE                                    #
+# --------------------------------------------------------------------------- #
+class DataTable(DataLayer):
+    """Creates and manages PostgreSQL data tables."""
 
+    def __init__(self, name):
+        super(DataTable, self).__init__(name)        
+
+    def create(self, df):
+        """Creates a PostgreSQL data table from a pandas DataFrame object.
         
+        Parameters
+        ----------
+        df : DataFrame
+            The pandas DataFrame to be written to the table.
+        """
